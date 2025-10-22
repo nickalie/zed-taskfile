ea83f21759f93ac5d11edcf856245eec66c0195c
Nick Gluschenko <nick@nickalie.com>
2025-10-22 21:56:21 +0300

    Improve Taskfile outline and task labels

--- languages/taskfile/outline.scm
+++ languages/taskfile/outline.scm
@@ -1,4 +1,9 @@
-;; Outline for Taskfile - show task names in the outline view
-;; Simplified to show all top-level keys
+; inherits: false
+;; Outline for Taskfile - show only task names under the "tasks" key
 (block_mapping_pair
-  key: (flow_node) @name) @item
+  key: (flow_node) @_tasks_key
+  value: (block_node
+    (block_mapping
+      (block_mapping_pair
+        key: (flow_node) @name) @item))
+  (#eq? @_tasks_key "tasks"))
